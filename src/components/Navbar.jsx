ed901fbfeac80790372e322b556e6a60c38a1b75
Mekel Tensley <mekelintech@gmail.com>
2022-04-25 13:38:59 -0400

    Initial commit. Create social timeline components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component{
+    render() {
+        return (
+            <div className="container-one">
+                <div className="title">Spacestagram</div>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
