91d2da2dd7f29d73e74926c3c990deb654f2f334
Mekel Tensley <mekelintech@gmail.com>
2022-04-29 12:11:01 -0400

    Complete structure of image card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,39 +36,3 @@ class Navbar extends Component {
 }
 
 export default Navbar;
-
-// class Navbar extends Component {
-//   render() {
-//     return (
-//       <div className="container-one">
-
-//         <div class="navbar">
-//         <div className="title">Spacestagram</div>
-//         <div class="navbar-item is-flex-touch">
-//             <a class="navbar-item">
-//               <i class="material-icons">explore</i>
-//             </a>
-//             <a class="navbar-item">
-//               <i class="material-icons">favorite_border</i>
-//             </a>
-//             <a class="navbar-item">
-//               <i class="material-icons">person_outline</i>
-//             </a>
-//           </div>
-//               <div class="search">
-//                 <input
-//                   class="input is-small has-text-centered"
-//                   type="text"
-//                   placeholder="search"
-//                 />
-//                 <span class="icon is-left is-small">
-//                   <i class="material-icons">search</i>
-//                 </span>
-//               </div>
-//           </div>
-//         </div>
-//     );
-//   }
-// }
-
-// export default Navbar;
