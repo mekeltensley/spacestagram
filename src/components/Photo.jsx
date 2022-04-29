bff556e60ce8eee896f5be84224aef790c0186e1
Mekel Tensley <mekelintech@gmail.com>
2022-04-29 19:03:10 -0400

    Add new elements to post image

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -7,21 +7,35 @@ class Photo extends Component {
     return (
       <article className="post" ref="post">
         <header>
-          <h3 className="image-title">[Image Name]</h3>
+          <div className="post-user">
+            <div className="post-user-profilepicture">
+              <img
+                src="https://images.unsplash.com/photo-1541873676-a18131494184?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmFzYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60"
+                alt="NASA Profile Picture"
+              />
+            </div>
+            <div className="post-user-username">
+              <span>@NASA</span>
+            </div>
+          </div>
         </header>
         <div className="post-image">
           <div className="post-image-bg">
-            <img
-              alt="The moon"
-              src="https://images.unsplash.com/photo-1616808830150-562e1d55778b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=454&q=80"
-              style= {{ width: '655px', height: '485px'}}
-
-            />
+            <figure class="hover-img">
+              <img
+                src="https://images.unsplash.com/photo-1616808830150-562e1d55778b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=454&q=80"
+                style={{ width: "655px", height: "485px" }}
+                alt="The Moon"
+              />
+              <figcaption>
+                <h3>
+                  [Photo Name]
+                </h3>
+                <br/>
+                <p>[Photo Details]</p>
+              </figcaption>
+            </figure>
           </div>
-        </div>
-        <div className="post-caption">
-          <strong>John D. Veloper </strong>
-          <p className="image-description">[Image Description]</p>
         </div>
       </article>
     );
