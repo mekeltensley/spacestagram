8b66d56283e2cb71de41aa12fd3cf61b1c19e3ad
Mekel Tensley <mekelintech@gmail.com>
2022-04-30 00:09:34 -0400

    Add new like button component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { Component } from "react";
-import Navbar from "./components/Navbar";
+import React, { Component, Fragment } from "react";
 import Photo from "./components/Photo";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <Photo />
+      < Router>
+      <div className="App" >
+        <Routes>
+          <Route path='/'
+            element={
+              <Fragment>
+                  <Navbar/>
+                  <Photo/>
+              </Fragment>
+            }
+          />
+        </Routes>
       </div>
+    </Router>
     );
   }
 }
---
+++ src/components/Like.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+
+const RippleButton = ({ children, onClick }) => {
+  const [coords, setCoords] = React.useState({ x: -1, y: -1 });
+  const [isRippling, setIsRippling] = React.useState(false);
+
+  React.useEffect(() => {
+    if (coords.x !== -1 && coords.y !== -1) {
+      setIsRippling(true);
+      setTimeout(() => setIsRippling(false), 300);
+    } else setIsRippling(false);
+  }, [coords]);
+
+  React.useEffect(() => {
+    if (!isRippling) setCoords({ x: -1, y: -1 });
+  }, [isRippling]);
+
+  return (
+    <button
+      className="ripple-button"
+      onClick={(e) => {
+        const rect = e.target.getBoundingClientRect();
+        setCoords({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+        onClick && onClick(e);
+      }}
+    >
+      {isRippling ? (
+        <span
+          className="ripple"
+          style={{
+            left: coords.x,
+            top: coords.y,
+          }}
+        />
+      ) : (
+        ""
+      )}
+      <span className="content">{children}</span>
+    </button>
+  );
+};
+
+class Like extends Component {
+  state = {
+    count: 0,
+  };
+
+  incrementLike = () => {
+    let newLike = this.state.count + 1;
+    this.setState({
+      count: newLike,
+    });
+  };
+
+  render() {
+    return (
+        
+        <RippleButton onClick={this.incrementLike}>
+        {this.state.count}
+      </RippleButton>
+
+    );
+  }
+}
+
+export default Like;
